493754a4db47c67038bda05485fe9f98c27079bc
Fatime668 <tu7dkjdue@code.edu.az>
2022-03-10 12:14:55 +0400

    Algoooo

---
+++ Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(addElement(num));
+        }
+        public static int addElement(int num)
+        {
+            int number = num;
+            int remainder = 0;
+            int newElement = 3;
+            while (num>0)
+            {
+                remainder = num % 10;
+                num /= 10;
+                newElement *= 10;
+
+            }
+            return newElement + number;
+        }
+       
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 10, 8, 98, 1, 100, 97, 2, 20 };
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
